7dac9d5fa06d33ee77056b38535d6c05079f0578
zz-james <zz.james@gmail.com>
2025-03-20 16:51:19 +0000

    experiments in responsiveness

---
+++ assets-do-not-upload/tiled/Windows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Windows" tilewidth="32" tileheight="32" tilecount="200" columns="20">
+ <image source="../Solaria_UI/World of Solaria UI Asset Pack Update 03a/PNG/Windows.png" width="640" height="336"/>
+</tileset>
