547cd754bb2c92fe6a14ea0e244804e7c1d5d202
James Smith <zz.james@gmail.com>
2025-04-08 13:48:33 +0100

    Delete assets-do-not-upload directory

--- assets-do-not-upload/tiled/environmant.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="environmant" tilewidth="32" tileheight="32" tilecount="56" columns="7">
- <image source="../Pato_Pack/Tileset/tileset_pato8.png" width="224" height="256"/>
-</tileset>
--- assets-do-not-upload/tiled/ground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="ground" tilewidth="32" tileheight="32" tilecount="1260" columns="10">
- <image source="../Game World Assets/Game World Assets/Ground/output.png" width="331" height="4043"/>
-</tileset>
